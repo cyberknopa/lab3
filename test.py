852631b8d731776e0f9c948543c862d73d33c490
Veronica Gavrilova <81513344+cyberknopa@users.noreply.github.com>
2025-01-15 08:39:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import unittest
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 
-class TestMNISTModel(unittest.TestCase):
 
+class TestMNISTModel(unittest.TestCase):
     
     def setUp(self):
         # Инициализация данных для тестов
         (self.x_train, self.y_train), (self.x_test, self.y_test) = \
-        tf.keras.datasets.mnist.load_data()
+            tf.keras.datasets.mnist.load_data()
         # Отбор 10% данных
         fraction = 0.1
         num_train_samples = int(len(self.x_train) * fraction)
@@ -16,10 +16,10 @@ class TestMNISTModel(unittest.TestCase):
 
         self.x_train_10 = self.x_train[:num_train_samples].astype('float32') / 255
         self.y_train_10 = \
-        tf.keras.utils.to_categorical(self.y_train[:num_train_samples], 10)
+            tf.keras.utils.to_categorical(self.y_train[:num_train_samples], 10)
         self.x_test_10 = self.x_test[:num_test_samples].astype('float32') / 255
         self.y_test_10 = \
-        tf.keras.utils.to_categorical(self.y_test[:num_test_samples], 10)
+            tf.keras.utils.to_categorical(self.y_test[:num_test_samples], 10)
 
     def test_data_shapes(self):
         # Проверка размеров данных
@@ -38,8 +38,9 @@ class TestMNISTModel(unittest.TestCase):
         model.compile(optimizer='adam',
                       loss='categorical_crossentropy',
                       metrics=['accuracy'])
-        history = model.fit(self.x_train_10, self.y_train_10, 
-                            epochs=1, batch_size=32, validation_data=(self.x_test_10, self.y_test_10))
+        history = model.fit(self.x_train_10, self.y_train_10,
+                            epochs=1, batch_size=32, 
+                            validation_data=(self.x_test_10, self.y_test_10))
         # Проверка, что после обучения хотя бы одна эпоха прошла
         self.assertGreater(len(history.history['loss']), 0)
         self.assertGreaterEqual(history.history['accuracy'][0], 0)
