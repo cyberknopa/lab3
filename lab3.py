45bb735b89be7e166a95c1245f86699121cbfdcb
Veronica Gavrilova <81513344+cyberknopa@users.noreply.github.com>
2025-01-15 08:44:21 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -21,7 +21,7 @@ data = scaler.fit_transform(data)
 
 # Разделение на обучающую и тестовую выборки
 X_train, X_test, y_train, y_test = \
-  train_test_split(data, target, test_size=0.2, random_state=42)
+    train_test_split(data, target, test_size=0.2, random_state=42)
 
 # Создание модели логистической регрессии
 model = LogisticRegression(max_iter=1000)
